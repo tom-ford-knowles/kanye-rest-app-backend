13554d0e84808935689558f60b40d3988f260c91
Tom Ford-Knowles <thomas.ford-knowles@hotmail.co.uk>
2024-05-15 14:33:57 +0100

    Adjust setup script and example env

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
+find . -type d -maxdepth 1 -exec sh -c 'if [ -e "{}"/.env.example ]; then cp "{}"/.env.example "{}"/.env; fi' \;
+
 docker run --rm \
     -u "$(id -u):$(id -g)" \
     -v "$(pwd):/var/www/html" \
     -w /var/www/html \
     laravelsail/php83-composer:latest \
     composer install --ignore-platform-reqs \
-    cat env.example > .env
+
+php artisan key:generate
